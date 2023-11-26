c2ccff2a7fd14e93eec8be11e457c8ba9391113f
jayagajjar <jaya.wired@gmail.com>
2023-11-27 09:24:29 +1100

    add emotions

--- app/components/PromptForm.tsx
+++ app/components/PromptForm.tsx
@@ -20,16 +20,16 @@ export default function PromptForm({ onSubmit, isLoading }: PromptFormProps){
         <br/>
         <label>AI does not have emotions of its own, but try adding some: </label>
        <select value={emotion} onChange={e=>{setEmotion(e.target.value)}}>
-<option value="anger">Anger</option>
-<option value="happy">Happy</option>
-<option value="sad">Sad</option>
-<option value="excited">Excited</option>
-<option value="satisfied">Satisfied</option>
-<option value="bored">Bored</option>
+        <option value="anger">Anger</option>
+        <option value="happy">Happy</option>
+        <option value="sad">Sad</option>
+        <option value="excited">Excited</option>
+        <option value="satisfied">Satisfied</option>
+        <option value="bored">Bored</option>
 
-</select>
-<br/>
-<br/>
+        </select>
+        <br/>
+        <br/>
         <label>Question</label>
         <input className={styles.input} type="text" value={prompt} onChange={e=>{setPrompt(e.target.value)}}></input>
         <input         className={styles.submitButton} type="submit" disabled={isLoading}/>
